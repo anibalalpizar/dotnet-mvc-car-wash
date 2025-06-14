b14ac2f9c234ec6b64b0100bbb215e077277669b
anibalalpizar <anibal.alpizar14@gmail.com>
2025-06-13 21:11:58 -0600

    Add CarWash class with validation and display attributes

---
+++ dotnet-mvc-car-wash/Models/CarWash.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+using System.Threading.Tasks;
+
+namespace dotnet_mvc_car_wash.Models
+{
+    public class CarWash
+    {
+        [Required]
+        [Display(Name = "Wash ID")]
+        public int WashId { get; set; }
+
+        [Required]
+        [Display(Name = "Vehicle License Plate")]
+        public string VehicleLicensePlate { get; set; }
+
+        [Required]
+        [Display(Name = "Customer ID")]
+        public string CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "Employee ID")]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        [Display(Name = "Wash Type")]
+        public WashType WashType { get; set; }
+
+        [Display(Name = "Base Price")]
+        public decimal BasePrice { get; set; }
+
+        [Display(Name = "IVA (13%)")]
+        public decimal IVA { get; set; }
+
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Wash Status")]
+        public WashStatus Status { get; set; }
+
+        [Display(Name = "Created Date")]
+        public DateTime CreatedDate { get; set; }
+    }
+}
