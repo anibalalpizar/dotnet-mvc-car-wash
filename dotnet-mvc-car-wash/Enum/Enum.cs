8c01be7e52bb562956c881367c1ae8734987ad4a
anibalalpizar <anibal.alpizar14@gmail.com>
2025-06-13 21:11:18 -0600

    Add enums for wash preferences, types, and statuses

---
+++ dotnet-mvc-car-wash/Enum/Enum.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public enum WashPreference
+{
+    [Display(Name = "Weekly")]
+    Weekly,
+    [Display(Name = "Biweekly")]
+    Biweekly,
+    [Display(Name = "Monthly")]
+    Monthly,
+    [Display(Name = "Other")]
+    Other
+}
+
+public enum WashType
+{
+    [Display(Name = "Basic")]
+    Basic = 8000,
+    [Display(Name = "Premium")]
+    Premium = 12000,
+    [Display(Name = "Deluxe")]
+    Deluxe = 20000,
+    [Display(Name = "La Joya")]
+    LaJoya = 0 
+}
+
+public enum WashStatus
+{
+    [Display(Name = "In Process")]
+    InProcess,
+    [Display(Name = "Billed")]
+    Billed,
+    [Display(Name = "Scheduled")]
+    Scheduled
+}
