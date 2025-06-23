be6caff9a11f93e8771700e42e7543558b6762ca
anibalalpizar <anibal.alpizar14@gmail.com>
2025-06-22 20:12:04 -0600

    Refactor enums for wash preferences and types

--- dotnet-mvc-car-wash/Enum/Enum.cs
+++ dotnet-mvc-car-wash/Enum/Enum.cs
@@ -1,35 +1,56 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public enum WashPreference
+namespace dotnet_mvc_car_wash.Models.Enums
 {
-    [Display(Name = "Weekly")]
-    Weekly,
-    [Display(Name = "Biweekly")]
-    Biweekly,
-    [Display(Name = "Monthly")]
-    Monthly,
-    [Display(Name = "Other")]
-    Other
-}
+    public enum TipoLavado
+    {
+        [Display(Name = "Básico")]
+        Basico,
+        Premium,
+        Deluxe,
+        [Display(Name = "La Joya")]
+        LaJoya
+    }
 
-public enum WashType
-{
-    [Display(Name = "Basic")]
-    Basic = 8000,
-    [Display(Name = "Premium")]
-    Premium = 12000,
-    [Display(Name = "Deluxe")]
-    Deluxe = 20000,
-    [Display(Name = "La Joya")]
-    LaJoya = 0
-}
+    public enum EstadoLavado
+    {
+        [Display(Name = "En Proceso")]
+        EnProceso,
+        Facturado,
+        Agendado
+    }
+
+    public enum WashType
+    {
+        [Display(Name = "Basic")]
+        Basic = 8000,
+        [Display(Name = "Premium")]
+        Premium = 12000,
+        [Display(Name = "Deluxe")]
+        Deluxe = 20000,
+        [Display(Name = "La Joya")]
+        LaJoya = 0
+    }
 
     public enum WashStatus
-{
-    [Display(Name = "In Process")]
-    InProcess,
-    [Display(Name = "Billed")]
-    Billed,
-    [Display(Name = "Scheduled")]
-    Scheduled
+    {
+        [Display(Name = "In Process")]
+        InProcess,
+        [Display(Name = "Billed")]
+        Billed,
+        [Display(Name = "Scheduled")]
+        Scheduled
+    }
+
+    public enum WashPreference
+    {
+        [Display(Name = "Semanal")]
+        Semanal,
+        [Display(Name = "Quincenal")]
+        Quincenal,
+        [Display(Name = "Mensual")]
+        Mensual,
+        [Display(Name = "Otro")]
+        Otro
+    }
 }
